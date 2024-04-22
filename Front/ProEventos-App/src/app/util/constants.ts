73646f6d33af5b491d7e7eff006dff91620a40aa
Eduardo Matias <eduardomatiasx@gmail.com>
2024-04-22 01:14:47 -0300

    Seção 7: Angular - Organizando rotas, alertas e mais

---
+++ Front/ProEventos-App/src/app/util/constants.ts
@@ -0,0 +1,25 @@
+export class Constants {
+    static DATE_TIME_FORMAT = 'dd/MM/yyyy hh:mm';
+    static DATE_TIME_FORMAT_API = 'yyyy-MM-ddTHH:mm:ss';
+    static DATE_FORMAT = 'dd/MM/yyyy';
+    static DATE_FORMAT_API = 'yyyy-MM-dd';
+    static TIME_FORMAT = 'HH:mm';
+    static TIME_FORMAT_API = 'HH:mm:ss';
+    static DATE_TIME_FORMAT_API_UTC = 'yyyy-MM-ddTHH:mm:ssZ';
+    static DATE_TIME_FORMAT_API_UTC_2 = 'yyyy-MM-ddTHH:mm:ss.SSSZ';
+    static DATE_TIME_FORMAT_API_UTC_3 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_4 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_5 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_6 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_7 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_8 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_9 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_10 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_11 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_12 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_13 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_14 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_15 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_16 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSSSSSSZ';
+    static DATE_TIME_FORMAT_API_UTC_17 = 'yyyy-MM-ddTHH:mm:ss.SSSSSSSSSSSSSSSSSSSSZ';
+}
